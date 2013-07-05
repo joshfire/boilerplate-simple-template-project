4e4ceec2d02ddf27f4b0a4302ba1083601147ddb
Arthur Jamain <arthur.jamain@gmail.com>
2013-07-05 11:57:19 +0200

    More comments

--- app/js/Router.js
+++ app/js/Router.js
@@ -22,39 +22,44 @@ define([
     init: function init (app) {
       this.app = app;
 
+      // Bind all the routes
       this.setRoutes();
+      // Override initial URL and set ours, because i'm lazy
       this.setInitialUrl();
+      // The actual start of the app
       this.historyStart();
     },
-
     setRoutes: function setRoutes () {
+      var ctrl;
       // Default routes
-      // These should always be called first
+      // These should always be called first in order for them to matter.
       this.setDefaultRoutes();
+
       // setup your custom routes here
 
       // DS-linked routes
       for(var k in this.app.controllers) {
-        this.route(this.app.controllers[k].slug, this.app.controllers[k].name, this.app.controllers[k].onNavigate);
-        this.route(this.app.controllers[k].slug + '/:index', this.app.controllers[k].name, this.app.controllers[k].onNavigateDetails);
+        ctrl = this.app.controllers[k];
+        // We're using the onNavigate and onNavigateDetails of each datasource
+        // as routes for their associated URLs.
+        this.route(ctrl.slug, ctrl.name, ctrl.onNavigate);
+        this.route(ctrl.slug + '/:index', ctrl.name, ctrl.onNavigateDetails);
       }
     },
-
     setDefaultRoutes: function setDefaultRoutes () {
       this.route('*path', 'Not Found Route', this.notfound);
       this.route('', 'Default Route', this.defaultRoute);
     },
-
     setInitialUrl: function setInitialUrl () {
       if (this.app.controllers && this.app.controllers.length) {
         document.location.hash = this.app.controllers[0].slug;
       }
     },
 
+
     defaultRoute: function defaultRoute () {
       logger.log('Default route hit');
     },
-
     notfound: function notfound () {
       logger.log('Route not found', arguments);
       this.navigate('', {trigger:true, replace:true});
--- app/js/app.js
+++ app/js/app.js
@@ -6,7 +6,10 @@
     L Toolbar
     L CardPanel
       L  n * datasource list view
-      L  n * details list view
+      L  n * datasource details view
+
+ * ... And then populates the main CardPanel with the DS views.
+ *
  */
 define([
   // Libs
@@ -65,7 +68,6 @@ define([
   var App = function(options) {
     this.initialize(options);
   };
-
   App.extend = Backbone.View.extend;
 
   App = App.extend({
@@ -81,22 +83,23 @@ define([
       this.initViews();
       logger.log('App init: Created views');
 
-      // Go !
+      /* This is the actual starting point of the app. The initRouter method
+      launches historystart which reads the current hash and executes the associated route.
+      While it is still inactive, we're free to set an initial route ourselves or to log in or whatever. */
       this.initRouter(options);
     },
 
     /**
-    * Init controllers
-    * @function
-    * Create a controller for each DS and defer the usage of said DS
-    * to the controller
-    **/
+     * Init controllers
+     * Create a controller for each DS. This controller will create the views
+     * associated with the DS and handle its routes.
+     */
     initControllers: function initControllers() {
       if ( window.Joshfire && window.Joshfire.factory ) {
         var datasources = window.Joshfire.factory.getDataSource('main').children;
         for(var k in datasources) {
-          // It shouldn't be hard to extend this controller so it expects to
-          // work with a certain @outputType
+          /* It should be fairly easy to create multiple versions of this controller
+          (extend it) for the various cases of OutputType we can get. */
           this.controllers[k] = new DatasourceController(this, {
             datasource: datasources[k],
             index: k,
@@ -108,18 +111,23 @@ define([
     },
 
     /**
-    * Init all the static views (at minima)
+    * Init all the static views (at minima) and the overall layout
     * @function
     **/
     initViews: function initViews () {
       var subViews = {};
-      // Main layout and toolbar, will never move
+
+      /* The main Layout, Toolbar and Content are the static views of the app ; they never move
+      and their behaviour stays the same. They won't need active treatment, so we can just keep
+      them around here for easy access. */
+
       this.views.layout = new Layout({
         el: '#app'
       });
       this.views.toolbar = new Toolbar({
         className: 'toolbar'
       });
+
       // If we're expecting content we use a cardpanel
       // Otherwise we'll set a simple item to show something anyway
       if (this.controllers.length) {
@@ -129,12 +137,16 @@ define([
       } else {
         this.views.content = new Item();
       }
-
+      // Sets the toolbar/content as the children of the layout.
       this.views.layout.setChildren({
         toolbar: this.views.toolbar,
         content: this.views.content
       });
 
+      // This is an important part : the app requests the views from each individual controller.
+      // In this case we go berserk mode and request them all at once even though we don't need em.
+      // Which is enough most of the time. But, any of the requested views couls easily be added whenever
+      // x or y happened using something along the lines of `views.content.addChild(controller.getView())`
       for (var k in this.controllers) {
         if(this.controllers.hasOwnProperty(k)) {
           subViews[this.controllers[k].slug] = this.controllers[k].getView();
@@ -142,34 +154,35 @@ define([
           this.views.toolbar.registerItem(this.controllers[k]);
         }
       }
-
+      // Set the children of the content view aaaaand...
       if (_.size(subViews) > 0) {
         this.views.content.setChildren(subViews);
       }
+      // Renders the whole tree of views
       this.views.layout.render();
     },
 
     /*
-    * Init router
-    * @function
-    **/
+     * Init router
+     * @function
+     */
     initRouter: function initRouter(options) {
       this.router = options.router;
       this.router.init(this);
       logger.log('Router set and initialized');
     },
-
+    /*
+     * Shows a list or a detail view depending on a controller
+     */
     showItem: function(item) {
-
       this.views.content.showChild(item.slug);
-
     },
     showItemDetails: function(item, index) {
-
       this.views.content.showChild(item.slug + '-details');
-
     },
-
+    /**
+     * Helper function to transform any latin string in a readable URL-ready string.
+     */
     slugify: function(str) {
       var slug = str.replace(/^\s+|\s+$/g, ''); // trim
       slug = slug.toLowerCase();
